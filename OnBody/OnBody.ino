e7d69dee9fc997dc9e5ef51f37b72e44c656baa2
Brett Dwyer <45947685+Brett797@users.noreply.github.com>
2025-03-27 16:54:44 -0500

    Added Temp Code and BLE Code
    Worked on this for a while. Should be good for a while, until the Pi cooperates

--- OnBody/OnBody.ino
+++ OnBody/OnBody.ino
@@ -1,20 +1,60 @@
 #include <Arduino.h>
-#include "BluetoothSerial.h"
+#include <ArduinoBLE.h>
 
 #include "driver/adc.h"
 #include "esp_adc_cal.h"
+// Temp Sensor Libs
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
-#define ADC_CHANNEL ADC1_CHANNEL_0  // GPIO 36 corresponds to ADC1_CHANNEL_0
+#define ADC_CHANNEL1 ADC1_CHANNEL_0  // GPIO 36 corresponds to ADC1_CHANNEL_0
+#define ADC_CHANNEL2 ADC1_CHANNEL_1  // GPIO 36 corresponds to ADC1_CHANNEL_0
 
 //BluetoothSerial SerialBT;  // Create a Bluetooth Serial object
 
+// Data wire is plugged into GPIO3 on the Arduino
+#define ONE_WIRE_BUS 3
+
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire);
+
+BLEService ADCService("180D");
+
+BLEFloatCharacteristic TempF("2A6E", BLERead | BLENotify);
+BLEFloatCharacteristic V1("2B18", BLERead | BLENotify);
+BLEFloatCharacteristic V2("2B19", BLERead | BLENotify);
+
+void BLESetup(){
+  if (!BLE.begin()) { 
+    Serial.println("starting BLE failed!");
+
+    while (1);
+  }
+  BLE.setDeviceName("ADC");
+  BLE.setLocalName("ADC");
+  BLE.setAdvertisedService(ADCService); // add the service UUID
+  ADCService.addCharacteristic(TempF); // add the battery level characteristic
+  ADCService.addCharacteristic(V1);
+  ADCService.addCharacteristic(V2);
+  BLE.addService(ADCService); // Add the battery service
+  TempF.writeValue(0); // set initial value for this characteristic
+  V1.writeValue(0);
+  V2.writeValue(0);
+  BLE.advertise();
+ // BLE.setConnectable(true);
+  Serial.println("BLE Started");
+}
 
 void configure_adc() {
     // Configure ADC width
     adc1_config_width(ADC_WIDTH_BIT_12);
     
     // Configure attenuation for proper voltage reading
-    adc1_config_channel_atten(ADC_CHANNEL, ADC_ATTEN_DB_11);
+    adc1_config_channel_atten(ADC_CHANNEL1, ADC_ATTEN_DB_11);
+    adc1_config_channel_atten(ADC_CHANNEL2, ADC_ATTEN_DB_11);
     
     // Apply ADC calibration
     esp_adc_cal_characteristics_t *adc_chars = 
@@ -29,30 +69,87 @@ void send_bluetooth_data(const char *data) {
 }
 
 void heartbeat_task() {
-    int adc_value = adc1_get_raw(ADC_CHANNEL); // Use ADC1 function correctly
-    Serial.print("Raw ADC Value: ");
-    Serial.println(adc_value);
+    int adc_value1 = adc1_get_raw(ADC_CHANNEL1); // Use ADC1 function correctly
+    int adc_value2 = adc1_get_raw(ADC_CHANNEL2); // Use ADC1 function correctly
+    Serial.print("Raw_ADC1_Value:");
+    Serial.print(adc_value1);
+    Serial.print(",");
+    float voltage1 = adc_value1 * (3.3 / 4095.0);
+    Serial.print("Voltage_1:");
+    Serial.print(voltage1, 3);
+    V1.writeValue(voltage1);
+    Serial.print(",");
+    Serial.print("Raw_ADC2_Value:");
+    Serial.print(adc_value2);
+    Serial.print(",");
+    float voltage2 = adc_value2 * (3.3 / 4095.0);
+    Serial.print("Voltage_2:");
+    Serial.println(voltage2, 3);
+    V2.writeValue(voltage2);
     
-    float voltage = adc_value * (3.3 / 4095.0);
-    Serial.print("Voltage: ");
-    Serial.println(voltage, 3);
-    
-    int heartbeat = (int)(voltage * 100);
+    /*int heartbeat = (int)(voltage * 100);
     Serial.print("Heartbeat: ");
-    Serial.println(heartbeat);
+    Serial.println(heartbeat);*/
     
-    delay(2000);
+    //delay(1000);
     }
 
+void temp_task(){
+  // call sensors.requestTemperatures() to issue a global temperature 
+  // request to all devices on the bus
+  //Serial.print("Requesting temperatures...");
+  sensors.requestTemperatures(); // Send the command to get temperatures
+  //Serial.println("DONE");
+  // After we got the temperatures, we can print them here.
+  // We use the function ByIndex, and as an example get the temperature from the first sensor only.
+  float temp = sensors.getTempFByIndex(0);
+
+  // Check if reading was successful
+  if(temp != -196.60) 
+  {
+    Serial.print("Temp:");
+    Serial.print(temp);
+    TempF.writeValue(temp);
+    Serial.print(",");
+  } 
+  else
+  {
+    Serial.println("Error: Could not read temperature data");
+  }
+}
 
 void setup() {
     Serial.begin(115200);
-    Serial.println("Bluetooth Started! Waiting for connection...");
+    //Serial.println("Bluetooth Started! Waiting for connection...");
     configure_adc();  // Initialize ADC here!
+    // Start up the Temp Sensor library
+    sensors.begin();
+    BLESetup();
 }
 
-
 void loop() {
-    heartbeat_task();
-    
+  BLEDevice central = BLE.central();
+  central.connected();
+  if (central) {
+    Serial.print("Connected to central: ");
+    // print the central's BT address:
+    Serial.println(central.address());
+    // turn on the LED to indicate the connection:
+    digitalWrite(LED_BUILTIN, HIGH);
+
+    // check the battery level every 200ms
+    // while the central is connected:
+    while (central.connected()) {
+      heartbeat_task();
+      temp_task();
+    }
+    // when the central disconnects, turn off the LED:
+    digitalWrite(LED_BUILTIN, LOW);
+    Serial.print("Disconnected from central: ");
+    Serial.println(central.address());
+  }
+  
+  //Serial.print("Local address is: ");
+
+  //Serial.println(BLE.address());
 }
