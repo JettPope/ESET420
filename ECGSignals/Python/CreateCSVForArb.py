15eadf14a1bb771bbcee4da8423367e0e0358590
Jett Pope <jettepope@gmail.com>
2025-04-22 14:52:37 -0500

    Add notes for developing Arb CSV creation

--- ECGSignals/Python/CreateCSVForArb.py
+++ ECGSignals/Python/CreateCSVForArb.py
@@ -6,8 +6,12 @@ scan_rate = 1000  # Hz
 channels = ["Output Filtered", "ECG", "D/A Output", "Digital Output 4", "Digital Output 6"]
 num_channels = len(channels) # 5 channels (length of channels array)
 
+# channels[1] is ECG channel and is the one we need
+
 data_type = np.float64 # DATATYPE FLOAT64 DO NOT CHANGE
 
 with open(file_name, "rb") as f:
     data = np.frombuffer(f.read(), dtype=data_type)
 
+# We need to count the total number of points and format the points according to the Arb's expected format and export to a CSV
+# It should be something like (point number, channels[1]) for each point
